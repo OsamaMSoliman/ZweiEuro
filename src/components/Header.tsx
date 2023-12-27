bc0a07574b600dfaed09a4a59dccb7363bf05dcc
Osama Soliman <Osama.M.Soliman@uni-bonn.de>
2023-12-27 15:49:00 +0100

    fixed Appbar sticky to top

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,35 +10,33 @@ export default function () {
     const [t] = useTranslation();
 
     return (
-        <header>
-            <Navbar expand="lg" className="bg-body-tertiary" fixed="top">
-                <Container fluid>
-                    <Navbar.Brand>
-                        <Link to="/">
-                            <Image src={logo} roundedCircle
-                                width="50" height="50"
-                                alt={t("app-nav-bar.title")} />
-                        </Link>
-                    </Navbar.Brand>
-                    <Navbar.Toggle />
-                    <Navbar.Collapse>
-                        <Nav variant="underline" defaultActiveKey="/">
-                            <Link to="/">{t("app-nav-bar.home-link")}</Link>
-                            <Link to="/table">{t("app-nav-bar.table-link")}</Link>
-                            <Link to="/grid">{t("app-nav-bar.grid-link")}</Link>
-                            <Link to="/upload">{t("app-nav-bar.upload-link")}</Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                    <Navbar.Collapse className="justify-content-end">
-                        <Navbar.Text>
-                            {t("app-nav-bar.sign-in-label")} :
-                            <Button variant="outline-dark ms-2" onClick={Logout} disabled={!user}>
-                                {user?.displayName}
-                            </Button>
-                        </Navbar.Text>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </header>
+        <Navbar expand="lg" className="bg-body-tertiary" sticky="top">
+            <Container fluid>
+                <Navbar.Brand>
+                    <Link to="/">
+                        <Image src={logo} roundedCircle
+                            width="50" height="50"
+                            alt={t("app-nav-bar.title")} />
+                    </Link>
+                </Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Collapse>
+                    <Nav variant="underline" defaultActiveKey="/">
+                        <Link to="/">{t("app-nav-bar.home-link")}</Link>
+                        <Link to="/table">{t("app-nav-bar.table-link")}</Link>
+                        <Link to="/grid">{t("app-nav-bar.grid-link")}</Link>
+                        <Link to="/upload">{t("app-nav-bar.upload-link")}</Link>
+                    </Nav>
+                </Navbar.Collapse>
+                <Navbar.Collapse className="justify-content-end">
+                    <Navbar.Text>
+                        {t("app-nav-bar.sign-in-label")} :
+                        <Button variant="outline-dark ms-2" onClick={Logout} disabled={!user}>
+                            {user?.displayName}
+                        </Button>
+                    </Navbar.Text>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
