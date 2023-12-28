4a7b66009f2ad79800eab728ae4c6c0d8b0ab8be
Osama Soliman <Osama.M.Soliman@uni-bonn.de>
2023-12-28 16:08:27 +0100

    fixing slidesheet merge conflict

--- src/components/SlideSheet.tsx
+++ src/components/SlideSheet.tsx
@@ -11,12 +11,16 @@ export const SlideSheet: React.FC<ICoinData> = ({ id, title, description, year, 
 
     return (
         <div style={{ height: `calc(100vh - ${HeaderHeight}px)` }}>
-            <Image src={img_uri} alt="image here" className='w-100 vh-100' style={{ objectFit: "cover" }} />
+            <Image src={img_uri} alt="image here" className='object-fit-contain w-100 h-100' />
             <CarouselCaption>
-                <h1>{title} - {year}</h1>
-                <h3>{description}</h3>
-                <h4>{CoinCategorys[category]}</h4>
-                <h4>{modifiedBy}</h4>
+                <div className='d-flex justify-content-center mb-3'>
+                    <div className='text-black-50 bg-secondary bg-gradient bg-opacity-25 rounded-5 p-3'>
+                        <h1>{title} - {year}</h1>
+                        <h3>{description}</h3>
+                        <h4>{CoinCategorys[category]}</h4>
+                        <h4>{modifiedBy}</h4>
+                    </div>
+                </div>
                 <LettersCardGroup id={id!} collection={collection} />
             </CarouselCaption>
         </div>
